1463816a3a158a9ebff63fb7315e1231dbea001a
ProDaddy <undefended0@gmail.com>
2020-03-10 09:33:28 +0530

    rendring messages with autoscroll functionality

--- client/src/components/Messages/Messages.jsx
+++ client/src/components/Messages/Messages.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import ScrollToBottom from 'react-scroll-to-bottom'
+
 import { Container } from '@material-ui/core'
 
-export default function Messages() {
+export default function Messages({messages}) {
     return (
         <Container style={{padding:'30px'}}>
-            pawan
+            <ScrollToBottom>
+                {messages.map((message,i)=>(
+                    <div key={i}>
+                        {message.message} - {message.name}
+                    </div>    
+                ))}
+            </ScrollToBottom>
         </Container>
     )
 }
